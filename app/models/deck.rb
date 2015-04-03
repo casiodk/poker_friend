baed62b0c4ae7a20d7d8e8e9a25c50d4b826fc41
Nicolai Seerup <mail@soundtracktor.com>
2015-04-03 18:11:04 +0200

    deck

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,16 +1,23 @@
-class Deck
-	attr_reader :card_array
-	
-	def initialize
-		@card_array = []
-		["s", "c", "d", "h"].each do |suite|
-			["A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"].each do |val|
-				@card_array << Card.new(suite: suite, val: val)
-			end
+class Deck	
+	class << self
+		def cards
+			@cards ||= generate_cards
 		end
-	end
 
-	def cards
-		@card_array
+		def shuffled_cards
+			cards.shuffle
+		end
+
+	private
+		def generate_cards
+			card_array = []
+			["s", "c", "d", "h"].each do |suite|
+				["A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"].each do |val|
+					card_array << Card.new(suite: suite, val: val)
+				end
+			end
+
+			card_array
+		end
 	end
 end
